a361f745b9999d86066b3cd255ae5b4a0c14d655
harimurillo <harimurillo@gmail.com>
2018-12-15 22:46:22 -0200

    End of free part of Mosh's course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
+import { EmailService } from './email.service';
+import { CoursesService } from './courses.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CoursesService,
+    EmailService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
